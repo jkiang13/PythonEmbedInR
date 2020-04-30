6f5c685ecacbff4cd16f7761daf601a96db28c9a
Jordan Kiang <jordan.kiang@sagebase.org>
2020-04-29 17:07:07 -0700

    3.7.7

--- tests/testthat/test_build.R
+++ tests/testthat/test_build.R
@@ -1,8 +1,8 @@
 context("test build")
 
-test_that("python version is 3.6.8", {
+test_that("python version is 3.7.7", {
   pyExec('import platform')
-  expect_equal('3.6.8', pyGet('platform.python_version()'))
+  expect_equal('3.7.7', pyGet('platform.python_version()'))
 })
 
 test_that("pip can be imported", {
